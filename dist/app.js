de6ec53202b973e4b8700fd22fabba97565f40bf
GrzeCHu-a11y <grzegorzgonczowski9@gmail.com>
2024-02-24 23:34:24 +0100

    added render func, stats in html and some styles

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,8 @@
 const items = document.querySelectorAll(".item");
+const statsContainer = document.querySelector(".stats-container");
+const h3PlayerChosenItem = document.querySelector(".player-chosen-item");
+const h3ComputerChosenItem = document.querySelector(".computer-chosen-item");
+const h2Status = document.querySelector(".status-h2");
 const options = ["scissors", "paper", "rock"];
 const getPlayerChoice = (e) => {
     const playerItem = e.target.id;
@@ -10,14 +14,21 @@ const computerChoice = (playerItem) => {
     checkWinner(computerItem, playerItem);
 };
 const checkWinner = (computerItem, playerItem) => {
-    console.log("computer", computerItem, "player", playerItem);
+    let status = "";
     if (playerItem === computerItem) {
-        console.log("draw");
+        status = "Draw";
     }
     else if (playerItem === "scissors" && computerItem === "rock" || playerItem === "paper" && computerItem === "scissors" || playerItem === "rock" && computerItem === "paper") {
-        console.log("player lose");
+        status = "Computer win";
     }
-    else
-        console.log("player win");
+    else {
+        status = "You win";
+    }
+    render(playerItem, computerItem, status);
+};
+const render = (playerItem, computerItem, status) => {
+    h3PlayerChosenItem.innerText = `You hose: ${playerItem}`;
+    h3ComputerChosenItem.innerText = `Computer hose: ${computerItem}`;
+    h2Status.innerText = status;
 };
 items.forEach((item) => item.addEventListener("click", getPlayerChoice));
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
 const items: NodeList = document.querySelectorAll(".item");
+const statsContainer: HTMLElement = document.querySelector(".stats-container");
+const h3PlayerChosenItem: HTMLElement = document.querySelector(".player-chosen-item");
+const h3ComputerChosenItem: HTMLElement = document.querySelector(".computer-chosen-item");
+const h2Status: HTMLElement = document.querySelector(".status-h2");
 
 const options: string[] = ["scissors", "paper", "rock"];
 
@@ -14,13 +18,24 @@ const computerChoice = (playerItem: string) => {
 }
 
 const checkWinner = (computerItem: string, playerItem: string) => {
-    console.log("computer", computerItem, "player", playerItem)
+
+    let status: string = "";
 
     if (playerItem === computerItem) {
-        console.log("draw")
+        status = "Draw";
     } else if (playerItem === "scissors" && computerItem === "rock" || playerItem === "paper" && computerItem === "scissors" || playerItem === "rock" && computerItem === "paper") {
-        console.log("player lose")
-    } else console.log("player win")
+        status = "Computer win";
+    } else {
+        status = "You win";
+    }
+
+    render(playerItem, computerItem, status)
+}
+
+const render = (playerItem: string, computerItem: string, status: string | null) => {
+    h3PlayerChosenItem.innerText = `You hose: ${playerItem}` 
+    h3ComputerChosenItem.innerText = `Computer hose: ${computerItem}` 
+    h2Status.innerText = status;
 }
 
 items.forEach((item) => item.addEventListener("click", getPlayerChoice))
