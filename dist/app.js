be725c7d155dfb8ec93ca608c2660bae12f24fcb
GrzeCHu-a11y <grzegorzgonczowski9@gmail.com>
2024-02-24 22:04:49 +0100

    added checkWinner func

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,23 @@
 const items = document.querySelectorAll(".item");
 const options = ["scissors", "paper", "rock"];
 const getPlayerChoice = (e) => {
-    const id = e.target.id;
-    computerChoice();
+    const playerItem = e.target.id;
+    computerChoice(playerItem);
 };
-const computerChoice = () => {
+const computerChoice = (playerItem) => {
     const randomIndex = Math.floor(Math.random() * 3);
-    console.log(options[randomIndex]);
+    const computerItem = options[randomIndex];
+    checkWinner(computerItem, playerItem);
+};
+const checkWinner = (computerItem, playerItem) => {
+    console.log("computer", computerItem, "player", playerItem);
+    if (playerItem === computerItem) {
+        console.log("draw");
+    }
+    else if (playerItem === "scissors" && computerItem === "rock" || playerItem === "paper" && computerItem === "scissors" || playerItem === "rock" && computerItem === "paper") {
+        console.log("player lose");
+    }
+    else
+        console.log("player win");
 };
 items.forEach((item) => item.addEventListener("click", getPlayerChoice));
--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,24 @@ const items: NodeList = document.querySelectorAll(".item");
 const options: string[] = ["scissors", "paper", "rock"];
 
 const getPlayerChoice = (e: MouseEvent) => {
-    const id: string | null = (e.target as HTMLElement).id
-    computerChoice()
+    const playerItem: string | null = (e.target as HTMLElement).id
+    computerChoice(playerItem)
 }
 
-const computerChoice = () => {
+const computerChoice = (playerItem: string) => {
     const randomIndex: number = Math.floor(Math.random() * 3)
-    console.log(options[randomIndex])
+    const computerItem = options[randomIndex];
+    checkWinner(computerItem, playerItem)
 }
 
+const checkWinner = (computerItem: string, playerItem: string) => {
+    console.log("computer", computerItem, "player", playerItem)
+
+    if (playerItem === computerItem) {
+        console.log("draw")
+    } else if (playerItem === "scissors" && computerItem === "rock" || playerItem === "paper" && computerItem === "scissors" || playerItem === "rock" && computerItem === "paper") {
+        console.log("player lose")
+    } else console.log("player win")
+}
 
 items.forEach((item) => item.addEventListener("click", getPlayerChoice))
